f2cb80485d42cca6d8b337092f486a63fcb69f9c
MarkPatkai <40295980+MarkPatkai@users.noreply.github.com>
2023-02-24 12:13:41 +0100

    redmage more strings added, needs test

--- src/main/java/io/fatefc/redmage/base/RedMageCard.java
+++ src/main/java/io/fatefc/redmage/base/RedMageCard.java
@@ -1,6 +1,11 @@
 package io.fatefc.redmage.base;
 
-public abstract class RedMageCard {
 
-    // TODO
+import static io.fatefc.redmage.RedMageInitializer.makeId;
+
+public abstract class RedMageCard extends CustomCard {
+    
+    public RedMageCard(final String id, final String name, final String img, final int cost, CardType cardType, CardRarity cardRarity, CardTarget cardTarget) {
+        super(makeId(id), name, img, cost, "NYI", cardType,  CardColor.RED, cardRarity, cardTarget);
+    }
 }
